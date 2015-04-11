217f86db3d59c5660748f20c6300acda7a7d2d35
ramatceler <ram@celerapps.com>
2015-04-12 00:44:30 +0530

    Sign in API & Update Password API.

--- Citrus/Citrus.SDK/Session.cs
+++ Citrus/Citrus.SDK/Session.cs
@@ -55,6 +55,45 @@ namespace Citrus.SDK
             return result != null;
         }
 
+        /// <summary>
+        /// Sign in the user account.
+        /// </summary>
+        /// <param name="userName">Citrus Pay UserName.</param>
+        /// <param name="password">Citrus Pay Password.</param>
+        /// <returns></returns>
+        public static async Task<bool> SigninUser(string userName, string password)
+        {
+            var request = new SigninRequest();
+            request.User = new User()
+            {
+                UserName = userName,
+                Password = password
+            };
+
+            RestWrapper rest = new RestWrapper();
+            var result = await rest.Post<object>(Service.Signin, request);
+
+            return result != null;
+        }
+
+        /// <summary>
+        /// Update your password with new password
+        /// </summary>
+        /// <param name="oldPassword">Your Old Password.</param>
+        /// <param name="newPassword">Your New Password.</param>
+        /// <returns></returns>
+        public static async Task<bool> UpdatePassword(string oldPassword, string newPassword)
+        {
+            var request = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("old", oldPassword),
+                new KeyValuePair<string, string>("new", oldPassword)
+            };
+            var rest = new RestWrapper();
+            var result = await rest.Post<object>(Service.UpdatePassword, request);
+            return result != null;
+        }
+
         public static async Task<bool> ResetPassword()
         {
             RestWrapper rest = new RestWrapper();
